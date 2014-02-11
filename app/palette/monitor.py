16dca97ab549a657f58e2b29b72bf846c2160422
Tim Flagg <timf@akirisolutions.com>
2014-02-11 10:50:19 -0800

    If there are multiple primary agents in the agents table, look to see if one appears to be connected and use that, instead of generating an error.

--- app/palette/monitor.py
+++ app/palette/monitor.py
@@ -9,6 +9,8 @@ import sys
 
 from akiri.framework.api import RESTApplication, DialogPage
 
+from controller.agentstatus import AgentStatusEntry
+
 from . import Session
 
 __all__ = ["MonitorApplication"]
@@ -29,37 +31,52 @@ class MonitorApplication(RESTApplication):
         secondary_state = "none"
 
         try:
-            primary_agent_entry = db_session.query(AgentStatusEntryAlt).\
-                filter(AgentStatusEntryAlt.agent_type == "primary").one()
+            primary_agents = db_session.query(AgentStatusEntry).\
+                filter(AgentStatusEntry.agent_type == "primary").all()
         except NoResultFound, e:
-            primary_agent_entry = None
+            primary_agents = None
 
-        # Get tableau status, main, and secondary states.
-        if primary_agent_entry:
-            if not primary_agent_entry.last_disconnect_time or \
-                    (primary_agent_entry.last_disconnect_time and \
-                            primary_agent_entry.last_disconnect_time <
-                                primary_agent_entry.last_connection_time):
+        # If there is more than one primary agent in the table, look for
+        # the primary agent that is connected and use that.
+        # the table.
 
-                # The primary agent is connected.
-                # Dig out the tableau status.
-                try:
-                    tableau_entry = db_session.query(StatusEntry).\
-                        filter(StatusEntry.name == 'Status').one()
-                    tableau_status = tableau_entry.status
-                except NoResultFound, e:
-                    pass
+        primary = None
 
-                # Dig out the states
-                state_entries = db_session.query(StateEntry).all()
-
-                for state_entry in state_entries:
-                    if state_entry.state_type == STATE_TYPE_MAIN:
-                        main_state = state_entry.state
-                    elif state_entry.state_type == STATE_TYPE_SECOND:
-                        secondary_state = state_entry.state
+        if primary_agents:
+            for agent in primary_agents:
+                if agent.last_disconnect_time:
+                    if agent.last_disconnect_time < agent.last_connection_time:
+                        # This agent has disconnected.
+                        continue
                     else:
-                        print "monitor: Uknown state_type:", state_entry.state_type
+                        # This primary agent is connected.  We will use it.
+                        primary = agent
+                        break
+                else:
+                    primary = agent
+                    break
+
+        # If there is a primary agent connected, get tableau status,
+        # main, and secondary states.
+        if primary:
+            # Dig out the tableau status.
+            try:
+                tableau_entry = db_session.query(StatusEntry).\
+                    filter(StatusEntry.name == 'Status').one()
+                tableau_status = tableau_entry.status
+            except NoResultFound, e:
+                pass
+
+            # Dig out the states
+            state_entries = db_session.query(StateEntry).all()
+
+            for state_entry in state_entries:
+                if state_entry.state_type == STATE_TYPE_MAIN:
+                    main_state = state_entry.state
+                elif state_entry.state_type == STATE_TYPE_SECOND:
+                    secondary_state = state_entry.state
+                else:
+                    print "monitor: Uknown state_type:", state_entry.state_type
 
         # Dig out the last/most recent backup.
         last_db = db_session.query(BackupEntry).\
@@ -80,27 +97,6 @@ class MonitorApplication(RESTApplication):
                 'secondary-state': secondary_state,
                 'last-backup': last_backup
                 }
-
-class AgentStatusEntryAlt(meta.Base):
-    __tablename__ = 'agents'
-
-    uuid = Column(String, primary_key=True)
-    hostname = Column(String)
-    agent_type = Column(String)
-    version = Column(String)
-    ip_address = Column(String)
-    listen_port = Column(Integer)
-    creation_time = Column(DateTime, default=func.now())
-    last_connection_time = Column(DateTime, default=func.now())
-    last_disconnect_time = Column(DateTime)
-
-    def __init__(self, hostname, agent_type, version, ip_address, listen_port, uuid):
-        self.hostname = hostname
-        self.agent_type = agent_type
-        self.version = version
-        self.ip_address = ip_address
-        self.listen_port = listen_port
-        self.uuid = uuid
 
 class BackupEntry(meta.Base):
     __tablename__ = 'backup'
