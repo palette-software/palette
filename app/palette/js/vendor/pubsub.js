b06fc58ed7372f3c18ae30672aa73cfef7f26bad
Matthew R. Laue <matt@akirisolutions.com>
2014-04-30 15:02:27 -0700

    Switch the pubsub implementation to: https://github.com/mroderick/PubSubJS, which works better with AMD.

--- app/palette/js/vendor/pubsub.js
+++ app/palette/js/vendor/pubsub.js
@@ -1,77 +1,215 @@
-/*	
+/*
+Copyright (c) 2010,2011,2012,2013 Morgan Roderick http://roderick.dk
+License: MIT - http://mrgnrdrck.mit-license.org
 
-	jQuery pub/sub plugin by Peter Higgins (dante@dojotoolkit.org)
+https://github.com/mroderick/PubSubJS
+*/
+/*jslint white:true, plusplus:true, stupid:true*/
+/*global
+	setTimeout,
+	module,
+	exports,
+	define,
+	require,
+	window
+*/
+(function(root, factory){
+	'use strict';
 
-	Loosely based on Dojo publish/subscribe API, limited in scope. Rewritten blindly.
+	// CommonJS
+	if (typeof exports === 'object' && module){
+		module.exports = factory();
 
-	Original is (c) Dojo Foundation 2004-2010. Released under either AFL or new BSD, see:
-	http://dojofoundation.org/license for more information.
+	// AMD
+	} else if (typeof define === 'function' && define.amd){
+		define(factory);
+	// Browser
+	} else {
+		root.PubSub = factory();
+	}
+}( ( typeof window === 'object' && window ) || this, function(){
 
-*/	
+	'use strict';
 
-;(function(d){
+	var PubSub = {},
+		messages = {},
+		lastUid = -1;
 
-	// the topic/subscription hash
-	var cache = {};
+	function hasKeys(obj){
+		var key;
 
-	d.publish = function(/* String */topic, /* Array? */args){
-		// summary: 
-		//		Publish some data on a named topic.
-		// topic: String
-		//		The channel to publish on
-		// args: Array?
-		//		The data to publish. Each array item is converted into an ordered
-		//		arguments on the subscribed functions. 
-		//
-		// example:
-		//		Publish stuff on '/some/topic'. Anything subscribed will be called
-		//		with a function signature like: function(a,b,c){ ... }
-		//
-		//	|		$.publish("/some/topic", ["a","b","c"]);
-		cache[topic] && d.each(cache[topic], function(){
-			this.apply(d, args || []);
-		});
-	};
-
-	d.subscribe = function(/* String */topic, /* Function */callback){
-		// summary:
-		//		Register a callback on a named topic.
-		// topic: String
-		//		The channel to subscribe to
-		// callback: Function
-		//		The handler event. Anytime something is $.publish'ed on a 
-		//		subscribed channel, the callback will be called with the
-		//		published array as ordered arguments.
-		//
-		// returns: Array
-		//		A handle which can be used to unsubscribe this particular subscription.
-		//	
-		// example:
-		//	|	$.subscribe("/some/topic", function(a, b, c){ /* handle data */ });
-		//
-		if(!cache[topic]){
-			cache[topic] = [];
-		}
-		cache[topic].push(callback);
-		return [topic, callback]; // Array
-	};
-
-	d.unsubscribe = function(/* Array */handle){
-		// summary:
-		//		Disconnect a subscribed function for a topic.
-		// handle: Array
-		//		The return value from a $.subscribe call.
-		// example:
-		//	|	var handle = $.subscribe("/something", function(){});
-		//	|	$.unsubscribe(handle);
-		
-		var t = handle[0];
-		cache[t] && d.each(cache[t], function(idx){
-			if(this == handle[1]){
-				cache[t].splice(idx, 1);
+		for (key in obj){
+			if ( obj.hasOwnProperty(key) ){
+				return true;
 			}
-		});
+		}
+		return false;
+	}
+
+	/**
+	 *	Returns a function that throws the passed exception, for use as argument for setTimeout
+	 *	@param { Object } ex An Error object
+	 */
+	function throwException( ex ){
+		return function reThrowException(){
+			throw ex;
+		};
+	}
+
+	function callSubscriberWithDelayedExceptions( subscriber, message, data ){
+		try {
+			subscriber( message, data );
+		} catch( ex ){
+			setTimeout( throwException( ex ), 0);
+		}
+	}
+
+	function callSubscriberWithImmediateExceptions( subscriber, message, data ){
+		subscriber( message, data );
+	}
+
+	function deliverMessage( originalMessage, matchedMessage, data, immediateExceptions ){
+		var subscribers = messages[matchedMessage],
+			callSubscriber = immediateExceptions ? callSubscriberWithImmediateExceptions : callSubscriberWithDelayedExceptions,
+			s;
+
+		if ( !messages.hasOwnProperty( matchedMessage ) ) {
+			return;
+		}
+
+		for (s in subscribers){
+			if ( subscribers.hasOwnProperty(s)){
+				callSubscriber( subscribers[s], originalMessage, data );
+			}
+		}
+	}
+
+	function createDeliveryFunction( message, data, immediateExceptions ){
+		return function deliverNamespaced(){
+			var topic = String( message ),
+				position = topic.lastIndexOf( '.' );
+
+			// deliver the message as it is now
+			deliverMessage(message, message, data, immediateExceptions);
+
+			// trim the hierarchy and deliver message to each level
+			while( position !== -1 ){
+				topic = topic.substr( 0, position );
+				position = topic.lastIndexOf('.');
+				deliverMessage( message, topic, data );
+			}
+		};
+	}
+
+	function messageHasSubscribers( message ){
+		var topic = String( message ),
+			found = Boolean(messages.hasOwnProperty( topic ) && hasKeys(messages[topic])),
+			position = topic.lastIndexOf( '.' );
+
+		while ( !found && position !== -1 ){
+			topic = topic.substr( 0, position );
+			position = topic.lastIndexOf( '.' );
+			found = Boolean(messages.hasOwnProperty( topic ) && hasKeys(messages[topic]));
+		}
+
+		return found;
+	}
+
+	function publish( message, data, sync, immediateExceptions ){
+		var deliver = createDeliveryFunction( message, data, immediateExceptions ),
+			hasSubscribers = messageHasSubscribers( message );
+
+		if ( !hasSubscribers ){
+			return false;
+		}
+
+		if ( sync === true ){
+			deliver();
+		} else {
+			setTimeout( deliver, 0 );
+		}
+		return true;
+	}
+
+	/**
+	 *	PubSub.publish( message[, data] ) -> Boolean
+	 *	- message (String): The message to publish
+	 *	- data: The data to pass to subscribers
+	 *	Publishes the the message, passing the data to it's subscribers
+	**/
+	PubSub.publish = function( message, data ){
+		return publish( message, data, false, PubSub.immediateExceptions );
 	};
 
-})(jQuery);
+	/**
+	 *	PubSub.publishSync( message[, data] ) -> Boolean
+	 *	- message (String): The message to publish
+	 *	- data: The data to pass to subscribers
+	 *	Publishes the the message synchronously, passing the data to it's subscribers
+	**/
+	PubSub.publishSync = function( message, data ){
+		return publish( message, data, true, PubSub.immediateExceptions );
+	};
 
+	/**
+	 *	PubSub.subscribe( message, func ) -> String
+	 *	- message (String): The message to subscribe to
+	 *	- func (Function): The function to call when a new message is published
+	 *	Subscribes the passed function to the passed message. Every returned token is unique and should be stored if
+	 *	you need to unsubscribe
+	**/
+	PubSub.subscribe = function( message, func ){
+		if ( typeof func !== 'function'){
+			return false;
+		}
+
+		// message is not registered yet
+		if ( !messages.hasOwnProperty( message ) ){
+			messages[message] = {};
+		}
+
+		// forcing token as String, to allow for future expansions without breaking usage
+		// and allow for easy use as key names for the 'messages' object
+		var token = 'uid_' + String(++lastUid);
+		messages[message][token] = func;
+
+		// return token for unsubscribing
+		return token;
+	};
+
+	/**
+	 *	PubSub.unsubscribe( tokenOrFunction ) -> String | Boolean
+	 *  - tokenOrFunction (String|Function): The token of the function to unsubscribe or func passed in on subscribe
+	 *  Unsubscribes a specific subscriber from a specific message using the unique token
+	 *  or if using Function as argument, it will remove all subscriptions with that function
+	**/
+	PubSub.unsubscribe = function( tokenOrFunction ){
+		var isToken = typeof tokenOrFunction === 'string',
+			result = false,
+			m, message, t, token;
+
+		for ( m in messages ){
+			if ( messages.hasOwnProperty( m ) ){
+				message = messages[m];
+
+				if ( isToken && message[tokenOrFunction] ){
+					delete message[tokenOrFunction];
+					result = tokenOrFunction;
+					// tokens are unique, so we can just stop here
+					break;
+				} else if (!isToken) {
+					for ( t in message ){
+						if (message.hasOwnProperty(t) && message[t] === tokenOrFunction){
+							delete message[t];
+							result = true;
+						}
+					}
+				}
+			}
+		}
+
+		return result;
+	};
+
+	return PubSub;
+}));
