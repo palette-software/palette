597dcf8fd8a8a846f59bdd314a56278e0f218c27
Patrick J. McEvoy <swagman@swagman.com>
2014-06-29 20:02:18 -0700

    Added a script directory and a scrip to drop tables.

---
+++ scripts/drop_tables.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# script to drop tables from postgres
+
+psql -U palette -W -d paldb -h localhost << EOF
+drop table s3 cascade;
+drop table extracts cascade;
+drop table workbooks_updates cascade;
+drop table workbooks cascade;
+drop table state_control cascade;
+drop table event_control cascade;
+drop table backup cascade;
+drop table state cascade;
+drop table status cascade;
+drop table XID cascade;
+drop table agent_info cascade;
+drop table agent_volumes cascade;
+drop table agent_yml cascade;
+drop table agent cascade;
+drop table role_permissions cascade;
+drop table permissions cascade;
+drop table role_roles cascade;
+drop table user_roles cascade;
+drop table roles cascade;
+drop table users cascade;
+drop table events cascade;
+drop table system cascade;
+drop table domain cascade;
+drop table apscheduler_jobs cascade;
+drop table environment cascade;
+drop table license cascade;
+drop table tableau_processes cascade;
+drop table firewall cascade;
+drop table gcs cascade;
+drop table data_connections cascade;
+drop table http_request cascade;
+drop table projects cascade;
+drop table sites cascade;
+drop table http_requests cascade;
+EOF
