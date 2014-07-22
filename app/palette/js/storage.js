5d80d02f5e3727b41f2be397313789a35e8ea2e4
Vahid Kowsari <vahid@palette-software.com>
2014-07-22 11:43:34 -0700

    Got server posting to work for text boxes

--- app/palette/js/storage.js
+++ app/palette/js/storage.js
@@ -1,56 +1,91 @@
 require(['jquery', 'template', 'common', 'EditBox', 'bootstrap'],
 function ($, template, common, EditBox)
 {
-    function ddCallback(node, value) {
-        var section = $(node).closest('section');
-        section.find('.admin-type').text('Palette '+value);
-    }
-
-    function update(data) {
-        $().ready(function() {
-            var t = $('#user-list-template').html();
-            var rendered = template.render(t, data);
-            $('#user-list').html(rendered);
-            $('div.dropdown').each(function () {
-                $(this).data('callback', ddCallback);
-            });
-            common.bindEvents();
-            common.setupDropdowns();
-            EditBox.setup();
-            $('#last-update').html(data['last-update']);
-        });
-    }
-
-    function refresh() {
-        $.ajax({
-            type: 'POST',
-            url: '/rest/users',
-            data: {'action': 'refresh'},
-            dataType: 'json',
-            async: false,
-
-            success: function(data) {
-                update(data);
-            },
-            error: common.ajaxError,
-        });
-    }
-
     common.startMonitor();
 
+    function update() {
+
+    }
+
     $.ajax({
-        url: '/rest/users',
+        url: '/rest/storage',
         success: function(data) {
             update(data);
         },
         error: common.ajaxError,
     });
 
+    /*
+     * customDataAttributes
+     * Return the HTML5 custom data attributes for a selector or domNode.
+     */
+    
+    function customDataAttributes(obj) {
+        if (obj instanceof $) {
+            obj = obj.get(0);
+        }
+        var d = {}
+        for (var i=0, attrs=obj.attributes, l=attrs.length; i<l; i++){
+            var name = attrs.item(i).nodeName;
+            if (!name.match('^data-')) {
+                continue;
+            }
+            d[name.substring(5)] = attrs.item(i).nodeValue;
+        }
+        return d;
+    }
+
+    /*
+     * getDataHREF
+     * Returns the HTML5 custom data attributes for an object or any of its parents
+     */
+    function getDataHREF(obj) {
+        var i = obj;
+        while (i) {
+            if (i.hasAttribute("data-href"))
+            {
+                return i.attributes['data-href'].value;
+            }
+            i = i.parentNode;
+        }
+    }
+
     $().ready(function() {
         $('.refresh > span').bind('click', function() {
             refresh();
         });
         $('.onoffswitch-inner').toggleClass("yesnoswitch-inner");
+
+        $('.numeric').bind("input", function () { 
+          this.value = this.value.replace(/[^0-9\.]/g,'');
+        });
+    
+        $('.data-enabled').bind("change", function() {
+            attributes = customDataAttributes(this);
+
+            name = attributes['name'];
+            url = getDataHREF(this);
+
+            if (name == "undefined" || url == "undefined") {
+                console.log('Error: data-name name or data-href');
+                return;
+            }
+
+            data = {'id': name, 'value': this.value | this.checked}; 
+
+            $.ajax({
+                type: 'POST',
+                url: url,
+                data: data,
+                dataType: 'json',
+                async: false,
+
+                success: function(data) {
+                    update(data);
+                },
+                error: common.ajaxError,
+            });
+        });
 
     });
 
--- app/palette/storage.py
+++ app/palette/storage.py
@@ -7,23 +7,64 @@ from controller.profile import UserProfile, Role, Publisher, Admin, License
 from controller.auth import AuthManager
 from controller.util import DATEFMT
 from controller.system import SystemEntry
+from controller.domain import Domain
+from controller.workbooks import WorkbookEntry, WorkbookUpdatesEntry
 
 from page import PalettePage
 from rest import PaletteRESTHandler, required_parameters, required_role
+
+import json
+
+__all__ = ["StorageApplication"]
+
+class StorageApplication(PaletteRESTHandler):
+    NAME = 'storage'
+
+    def __init__(self, global_conf):
+        super(StorageApplication, self).__init__(global_conf)
+
+    def handle_get(self, req):
+        query = meta.Session.query(SystemEntry).all()
+
+        storage = []
+        for entry in query:
+            item = {}
+            item[entry.key] = entry.value
+            storage.append(item)
+
+        return {'storage': storage}
+
+    @required_role(Role.MANAGER_ADMIN)
+    @required_parameters('id', 'value')
+    def handle_post(self, req):
+        key = req.POST['id']
+        value = req.POST['value']
+        if key == "main_warning":
+            pass
+        elif key == "main_error":
+            pass
+        elif key == "other_warning":
+            pass
+        elif key == "other_error":
+            pass        
+        return {}
+
+    def handle(self, req):
+        if req.method == "GET":
+            return self.handle_get(req)
+        if req.method == "POST":
+            return self.handle_post(req)
+        else:
+            raise exc.HTTPBadRequest()
 
 class StoragePage(PalettePage):
     TEMPLATE = "storage.mako"
     active = 'storage'
     expanded = True
-    required_role = Role.READONLY_ADMIN
-    
-    def __init__(self, global_conf):
-        super(StoragePage, self).__init__(global_conf)       
-        #query = meta.Session.query(SystemEntry)
-        #print query
+    required_role = Role.MANAGER_ADMIN
 
-    def render(self, req, obj=None):        
-        return super(StoragePage, self).render(req, obj=obj)
+    def handle(self, req):
+        return None
 
 def make_storage(global_conf):
     return StoragePage(global_conf)
