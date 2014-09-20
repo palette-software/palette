a4c9ef396088114408ae0bbd3c2517738cb50b67
Tim Flagg <timf@akirisolutions.com>
2014-09-20 15:45:22 -0700

    pylint for event_control.py

--- controller/controller/event_control.py
+++ controller/controller/event_control.py
@@ -1,12 +1,11 @@
 import sys, traceback
-import time
 import datetime
 from mako.template import Template
 from mako import exceptions
+from mako.exceptions import MakoException
 import mako.runtime
 
-from sqlalchemy import Column, Integer, BigInteger, String, Boolean
-from sqlalchemy.schema import ForeignKey, UniqueConstraint
+from sqlalchemy import Column, BigInteger, String, Boolean
 from sqlalchemy.orm.exc import NoResultFound
 from akiri.framework.ext.sqlalchemy import meta
 
@@ -19,7 +18,7 @@ from manager import Manager
 from sites import Site
 from projects import Project
 
-mako.runtime.UNDEFINED=UNDEFINED
+mako.runtime.UNDEFINED = UNDEFINED
 
 import re
 
@@ -58,110 +57,111 @@ class EventControl(meta.Base, BaseMixin):
     event_type = Column(String) # backup, restore, etc.
 
     # event keys
-    INIT_STATE_STARTED="INIT-STATE-STARTED"
-    INIT_STATE_STOPPED="INIT-STATE-STOPPED"
-    INIT_STATE_DEGRADED="INIT-STATE-DEGRADED"
+    INIT_STATE_STARTED = "INIT-STATE-STARTED"
+    INIT_STATE_STOPPED = "INIT-STATE-STOPPED"
+    INIT_STATE_DEGRADED = "INIT-STATE-DEGRADED"
 
-    STATE_STARTED="STATE-STARTED"
-    STATE_STOPPED="STATE-STOPPED"
-    STATE_DEGRADED="STATE-DEGRADED"
-    STATE_STARTED_AFTER_DEGRADED="STATE-STARTED-AFTER-DEGRADED"
-    STATE_UNEXPECTED_STATE_STOPPED="STATE-UNEXPECTED-STOPPED"
-    STATE_UNEXPECTED_STATE_STARTED="STATE-UNEXPECTED-STARTED"
-    STATE_UNEXPECTED_STOPPED_AFTER_DEGRADED=\
+    STATE_STARTED = "STATE-STARTED"
+    STATE_STOPPED = "STATE-STOPPED"
+    STATE_DEGRADED = "STATE-DEGRADED"
+    STATE_STARTED_AFTER_DEGRADED = "STATE-STARTED-AFTER-DEGRADED"
+    STATE_UNEXPECTED_STATE_STOPPED = "STATE-UNEXPECTED-STOPPED"
+    STATE_UNEXPECTED_STATE_STARTED = "STATE-UNEXPECTED-STARTED"
+    # pylint: disable=invalid-name
+    STATE_UNEXPECTED_STOPPED_AFTER_DEGRADED = \
                                     "STATE-UNEXPECTED-STOPPED-AFTER-DEGRADED"
 
-    BACKUP_STARTED="BACKUP-STARTED"
-    BACKUP_FINISHED="BACKUP-FINISHED"
-    BACKUP_FINISHED_COPY_FAILED="BACKUP-FINISHED-COPY-FAILED"
-    BACKUP_FAILED="BACKUP-FAILED"
+    BACKUP_STARTED = "BACKUP-STARTED"
+    BACKUP_FINISHED = "BACKUP-FINISHED"
+    BACKUP_FINISHED_COPY_FAILED = "BACKUP-FINISHED-COPY-FAILED"
+    BACKUP_FAILED = "BACKUP-FAILED"
 
-    BACKUP_STARTED_SCHEDULED="BACKUP-STARTED-SCHEDULED"
-    BACKUP_FINISHED_SCHEDULED="BACKUP-FINISHED-SCHEDULED"
-    BACKUP_FINISHED_SCHEDULED_COPY_FAILED=\
+    BACKUP_STARTED_SCHEDULED = "BACKUP-STARTED-SCHEDULED"
+    BACKUP_FINISHED_SCHEDULED = "BACKUP-FINISHED-SCHEDULED"
+    BACKUP_FINISHED_SCHEDULED_COPY_FAILED = \
                                         "BACKUP-FINISHED-SCHEDULED-COPY-FAILED"
-    BACKUP_FAILED_SCHEDULED="BACKUP-FAILED-SCHEDULED"
+    BACKUP_FAILED_SCHEDULED = "BACKUP-FAILED-SCHEDULED"
 
-    BACKUP_BEFORE_STOP_STARTED="BACKUP-BEFORE-STOP-STARTED"
-    BACKUP_BEFORE_STOP_FINISHED="BACKUP-BEFORE-STOP-FINISHED"
-    BACKUP_BEFORE_STOP_FINISHED_COPY_FAILED=\
+    BACKUP_BEFORE_STOP_STARTED = "BACKUP-BEFORE-STOP-STARTED"
+    BACKUP_BEFORE_STOP_FINISHED = "BACKUP-BEFORE-STOP-FINISHED"
+    BACKUP_BEFORE_STOP_FINISHED_COPY_FAILED = \
                                     "BACKUP-BEFORE-STOP-FINISHED-COPY-FAILED"
-    BACKUP_BEFORE_STOP_FAILED="BACKUP-BEFORE-STOP-FAILED"
+    BACKUP_BEFORE_STOP_FAILED = "BACKUP-BEFORE-STOP-FAILED"
 
-    BACKUP_BEFORE_RESTORE_STARTED="BACKUP-BEFORE-RESTORE-STARTED"
-    BACKUP_BEFORE_RESTORE_FINISHED="BACKUP-BEFORE-RESTORE-FINISHED"
-    BACKUP_BEFORE_RESTORE_FINISHED_COPY_FAILED=\
+    BACKUP_BEFORE_RESTORE_STARTED = "BACKUP-BEFORE-RESTORE-STARTED"
+    BACKUP_BEFORE_RESTORE_FINISHED = "BACKUP-BEFORE-RESTORE-FINISHED"
+    BACKUP_BEFORE_RESTORE_FINISHED_COPY_FAILED = \
                                 "BACKUP-BEFORE-RESTORE-FINISHED-COPY-FAILED"
-    BACKUP_BEFORE_RESTORE_FAILED="BACKUP-BEFORE-RESTORE-FAILED"
+    BACKUP_BEFORE_RESTORE_FAILED = "BACKUP-BEFORE-RESTORE-FAILED"
 
-    RESTORE_STARTED="RESTORE-STARTED"
-    RESTORE_FINISHED="RESTORE-FINISHED"
-    RESTORE_FAILED="RESTORE-FAILED"
+    RESTORE_STARTED = "RESTORE-STARTED"
+    RESTORE_FINISHED = "RESTORE-FINISHED"
+    RESTORE_FAILED = "RESTORE-FAILED"
 
-    TABLEAU_START_FAILED="TABLEAU-START-FAILED"
+    TABLEAU_START_FAILED = "TABLEAU-START-FAILED"
 
-    MAINT_START_FAILED="MAINT-START-FAILED"
-    MAINT_STOP_FAILED="MAINT-STOP-FAILED"
+    MAINT_START_FAILED = "MAINT-START-FAILED"
+    MAINT_STOP_FAILED = "MAINT-STOP-FAILED"
 
-    MAINT_OFFLINE="MAINT-OFFLINE"
-    MAINT_ONLINE="MAINT-ONLINE"
+    MAINT_OFFLINE = "MAINT-OFFLINE"
+    MAINT_ONLINE = "MAINT-ONLINE"
 
-    ARCHIVE_START_FAILED="ARCHIVE-START-FAILED"
-    ARCHIVE_STOP_FAILED="ARCHIVE-STOP-FAILED"
+    ARCHIVE_START_FAILED = "ARCHIVE-START-FAILED"
+    ARCHIVE_STOP_FAILED = "ARCHIVE-STOP-FAILED"
 
-    AGENT_COMM_LOST="AGENT-COMM-LOST"
-    AGENT_FAILED_STATUS="AGENT-FAILED-STATUS"
-    AGENT_RETURNED_INVALID_STATUS="AGENT-RETURNED-INVALID-STATUS"
+    AGENT_COMM_LOST = "AGENT-COMM-LOST"
+    AGENT_FAILED_STATUS = "AGENT-FAILED-STATUS"
+    AGENT_RETURNED_INVALID_STATUS = "AGENT-RETURNED-INVALID-STATUS"
 
-    AGENT_DISCONNECT="AGENT-DISCONNECT"
-
-    #
-    LICENSE_INVALID="LICENSE-INVALID"
-    LICENSE_EXPIRED="LICENSE-EXPIRED"
-    LICENSE_REPAIR_STARTED="LICENSE-REPAIR-STARTED"
-    LICENSE_REPAIR_FINISHED="LICENSE-REPAIR-FINISHED"
-    LICENSE_REPAIR_FAILED="LICENSE-REPAIR-FAILED"
-
-    PERMISSION="PERMISSION"
-    AGENT_COMMUNICATION="AGENT-COMMUNICATION"
-    MAINT_WEB="MAINT-WEB"
-    TABLEAU_USER_TABLE="TABLEAU-USER-TABLE"
-    TABLEAU_SYSTEM_TABLE="TABLEAU-SYSTEM-TABLE"
-
-    SCHEDULED_JOB_STARTED="SCHEDULE-JOB-STARTED"
-    SCHEDULED_JOB_FAILED="SCHEDULE-JOB-FAILED"
+    AGENT_DISCONNECT = "AGENT-DISCONNECT"
 
     #
-    EXTRACT_OK="EXTRACT-OK"
-    EXTRACT_FAILED="EXTRACT-FAILED"
+    LICENSE_INVALID = "LICENSE-INVALID"
+    LICENSE_EXPIRED = "LICENSE-EXPIRED"
+    LICENSE_REPAIR_STARTED = "LICENSE-REPAIR-STARTED"
+    LICENSE_REPAIR_FINISHED = "LICENSE-REPAIR-FINISHED"
+    LICENSE_REPAIR_FAILED = "LICENSE-REPAIR-FAILED"
 
-    ZIPLOGS_STARTED="ZIPLOGS-STARTED"
-    ZIPLOGS_FINISHED="ZIPLOGS-FINISHED"
-    ZIPLOGS_FAILED="ZIPLOGS-FAILED"
+    PERMISSION = "PERMISSION"
+    AGENT_COMMUNICATION = "AGENT-COMMUNICATION"
+    MAINT_WEB = "MAINT-WEB"
+    TABLEAU_USER_TABLE = "TABLEAU-USER-TABLE"
+    TABLEAU_SYSTEM_TABLE = "TABLEAU-SYSTEM-TABLE"
 
-    HTTP_BAD_STATUS="HTTP-BAD-STATUS"
-    HTTP_LOAD_WARN="HTTP-LOAD-WARN"
-    HTTP_LOAD_ERROR="HTTP-LOAD-ERROR"
+    SCHEDULED_JOB_STARTED = "SCHEDULE-JOB-STARTED"
+    SCHEDULED_JOB_FAILED = "SCHEDULE-JOB-FAILED"
 
-    CLEANUP_STARTED="CLEANUP-STARTED"
-    CLEANUP_FINISHED="CLEANUP-FINISHED"
-    CLEANUP_FAILED="CLEANUP-FAILED"
-    SYNC_FAILED="SYNC_FAILED"
+    #
+    EXTRACT_OK = "EXTRACT-OK"
+    EXTRACT_FAILED = "EXTRACT-FAILED"
 
-    FIREWALL_OPEN_FAILED="FIREWALL-OPEN-FAILED"
-    PORT_CONNECTION_FAILED="PORT-CONNECTION-FAILED"
-    PORT_CONNECTION_OKAY="PORT-CONNECTION-OKAY"
+    ZIPLOGS_STARTED = "ZIPLOGS-STARTED"
+    ZIPLOGS_FINISHED = "ZIPLOGS-FINISHED"
+    ZIPLOGS_FAILED = "ZIPLOGS-FAILED"
 
-    DISK_USAGE_ABOVE_HIGH_WATERMERK="DISK-USAGE-ABOVE-HIGH-WATERMARK"
-    DISK_USAGE_ABOVE_LOW_WATERMARK="DISK-USAGE-ABOVE-LOW-WATERMARK"
-    DISK_USAGE_OKAY="DISK-USAGE-OKAY"
+    HTTP_BAD_STATUS = "HTTP-BAD-STATUS"
+    HTTP_LOAD_WARN = "HTTP-LOAD-WARN"
+    HTTP_LOAD_ERROR = "HTTP-LOAD-ERROR"
 
-    EMAIL_TEST="EMAIL-TEST"
+    CLEANUP_STARTED = "CLEANUP-STARTED"
+    CLEANUP_FINISHED = "CLEANUP-FINISHED"
+    CLEANUP_FAILED = "CLEANUP-FAILED"
+    SYNC_FAILED = "SYNC_FAILED"
+
+    FIREWALL_OPEN_FAILED = "FIREWALL-OPEN-FAILED"
+    PORT_CONNECTION_FAILED = "PORT-CONNECTION-FAILED"
+    PORT_CONNECTION_OKAY = "PORT-CONNECTION-OKAY"
+
+    DISK_USAGE_ABOVE_HIGH_WATERMERK = "DISK-USAGE-ABOVE-HIGH-WATERMARK"
+    DISK_USAGE_ABOVE_LOW_WATERMARK = "DISK-USAGE-ABOVE-LOW-WATERMARK"
+    DISK_USAGE_OKAY = "DISK-USAGE-OKAY"
+
+    EMAIL_TEST = "EMAIL-TEST"
 
     # levels
-    LEVEL_ERROR="E"
-    LEVEL_WARNING="W"
-    LEVEL_INFO="I"
+    LEVEL_ERROR = "E"
+    LEVEL_WARNING = "W"
+    LEVEL_INFO = "I"
 
     level_strings = {
         LEVEL_ERROR: "Error",
@@ -185,7 +185,7 @@ class EventControl(meta.Base, BaseMixin):
         'restore' : 'Tableau Restore',
         'server' : 'Palette Server',
         'storage' : 'Storage',
-        'tableau' : 'Tableau App' }
+        'tableau' : 'Tableau App'}
 
     @classmethod
     def types(cls):
@@ -207,12 +207,17 @@ class EventControlManager(Manager):
         try:
             entry = meta.Session.query(EventControl).\
                 filter(EventControl.key == key).one()
-        except NoResultFound, e:
+        except NoResultFound:
             return None
 
         return entry
 
-    def gen(self, key, data={}, userid=None, siteid=None, timestamp=None):
+    def gen(self, key, data=None, userid=None, siteid=None, timestamp=None):
+        # pylint: disable=too-many-arguments
+        # pylint: disable=too-many-locals
+        # pylint: disable=too-many-statements
+        # pylint: disable=too-many-branches
+
         """Generate an event.
             Arguments:
                 key:    The key to look up.
@@ -242,6 +247,9 @@ class EventControlManager(Manager):
                                     install-dir
         """
 
+        if data == None:
+            data = {}
+
         event_entry = self.get_event_control_entry(key)
         if event_entry:
             subject = event_entry.subject
@@ -252,7 +260,7 @@ class EventControlManager(Manager):
 
         # add all system table entries to the data dictionary.
         data = dict(data.items() + self.server.system.todict().items())
-        
+
         self.log.debug(key + " DATA: " + str(data))
 
         if 'exit-status' in data:
@@ -306,8 +314,8 @@ class EventControlManager(Manager):
         # Use the data dict for template substitution.
         try:
             subject = subject % data
-        except (ValueError, KeyError) as e:
-            subject = "Template subject conversion failure: " + str(e) + \
+        except (ValueError, KeyError) as ex:
+            subject = "Template subject conversion failure: " + str(ex) + \
                       "subject: " + subject + \
                       ", data: " + str(data)
 
@@ -315,14 +323,14 @@ class EventControlManager(Manager):
             try:
                 mako_template = Template(event_description)
                 event_description = mako_template.render(**data)
-            except:
+            except MakoException:
                 event_description = \
                     "Mako template message conversion failure: " + \
                         exceptions.text_error_template().render() + \
                             "\ntemplate: " + event_description + \
                             "\ndata: " + str(data)
         else:
-           event_description = self.make_default_description(data)
+            event_description = self.make_default_description(data)
 
         event_description = re.sub("(\n|\r\n){3,}", "\n\n", event_description)
         if not event_description.endswith("\n"):
@@ -347,10 +355,11 @@ class EventControlManager(Manager):
         if event_entry.send_email:
             try:
                 self.alert_email.send(event_entry, data)
-            except Exception, e:
-                exc_type, exc_value, exc_traceback = sys.exc_info()
-                tb = ''.join(traceback.format_tb(exc_traceback))
-                report = "Error: %s.  Traceback: %s" % (sys.exc_info()[1], tb)
+            except StandardError:
+                exc_traceback = sys.exc_info()[2]
+                tback = ''.join(traceback.format_tb(exc_traceback))
+                report = "Error: %s.  Traceback: %s" % (sys.exc_info()[1],
+                                                        tback)
 
                 self.log.error(("alert_email: Failed for event '%s', ' + \
                                  data '%s'.  Will not send email. %s"),
