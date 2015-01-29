a0184fc031d905c31202b1c8e33e4297807f3ec9
Matthew R. Laue <matt@akirisolutions.com>
2015-01-29 10:51:51 -0800

    Implement the trial days remaining style.
    This checkin also includes a fix to the broken SSL enable slide on the setup page.

--- scripts/start_trial.sh
+++ scripts/start_trial.sh
@@ -2,7 +2,7 @@
 # script to drop tables from postgres
 
 psql -U palette -d paldb -h localhost << EOF
-UPDATE domain SET expiration_time = NOW() + INTERVAL '21 days';
+UPDATE domain SET expiration_time = NOW() + INTERVAL '14 days';
 UPDATE domain SET contact_time = NOW();
 UPDATE domain SET trial = TRUE;
 EOF
