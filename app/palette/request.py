28334cf5ea2ec60cc53f0eb41af94be2ffe17696
Matthew R. Laue <matt@akirisolutions.com>
2015-03-23 13:11:54 -0700

    Forcible reload the env and domain object per request (only needed for the development server).

--- app/palette/request.py
+++ app/palette/request.py
@@ -106,12 +106,10 @@ def req_getattr(req, name):
     if name == 'envid':
         return req.palette_environment.envid
     if name == 'palette_environment': # to be consistent with palette_domain
-        if not ENVIRON_ENVIRONMENT_NAME in req.environ:
-            req.environ[ENVIRON_ENVIRONMENT_NAME] = Environment.get()
+        req.environ[ENVIRON_ENVIRONMENT_NAME] = Environment.get()
         return req.environ[ENVIRON_ENVIRONMENT_NAME]
     if name == 'palette_domain': # webob already has a 'domain' property.
-        if not ENVIRON_DOMAIN_NAME in req.environ:
-            req.environ[ENVIRON_DOMAIN_NAME] = Domain.getone()
+        req.environ[ENVIRON_DOMAIN_NAME] = Domain.getone()
         return req.environ[ENVIRON_DOMAIN_NAME]
     if name == 'system':
         return System(req.envid)
