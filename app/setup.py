557f03621099b54bdea3706c5bac3beee7682ef1
Tim Flagg <timf@akirisolutions.com>
2015-08-26 18:22:34 -0700

    Bump version to 1.6.0

--- app/setup.py
+++ app/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.5.2'
+version = '1.6.0'
 
 setup(name='palette',
     version=version,
--- controller/setup.py
+++ controller/setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-VERSION='1.5.2'
+VERSION='1.6.0'
 
 setup(name='controller',
       version=VERSION,
