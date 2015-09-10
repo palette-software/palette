51201d1ed0bc11a3fee5006d06ae89541959c97c
Matthew R. Laue <matt@akirisolutions.com>
2015-09-10 14:02:30 -0700

    Update the controller script use the new package functionality based on __main__.py.

--- controller/controller/__main__.py
+++ controller/controller/__main__.py
@@ -1,8 +1,12 @@
+""" Script execution of the module """
+# pylint: enable=missing-docstring,relative-import
+import sys
 import os
+
 from .controller import main
 
 try:
-    main()
+    sys.exit(main())
 except KeyboardInterrupt:
     print "\nInterrupted.  Exiting."
     # pylint: disable=protected-access
