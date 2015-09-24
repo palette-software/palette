7befcf9122cce23d14631db6e7e5d90ae30e6419
Tim Flagg <timf@akirisolutions.com>
2015-09-24 15:38:48 -0700

    Bug fix from system table refactoring.

--- controller/controller/alert_email.py
+++ controller/controller/alert_email.py
@@ -2,15 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import time
 import traceback
 import smtplib
-import datetime
+from datetime import datetime
+import threading
 from email.mime.text import MIMEText
 from email.header import Header
 from sqlalchemy.orm.exc import NoResultFound
 
 import akiri.framework.sqlalchemy as meta
 
+from agent import Agent
 from domain import Domain
 from event_control import EventControl
 from email_limit import EmailLimitManager
@@ -227,14 +230,14 @@ class AlertEmail(object):
                                    EventControl.EMAIL_TEST]:
             entry = Domain.getone()
             if entry.expiration_time and \
-                            datetime.datetime.utcnow() > entry.expiration_time:
+                            datetime.utcnow() > entry.expiration_time:
                 self.log.debug("License expired. " +
                                     "Not sending: Subject: %s, Message: %s",
                                     subject, message)
                 return
 
             if entry.contact_time:
-                silence_time = (datetime.datetime.utcnow() - \
+                silence_time = (datetime.utcnow() - \
                                         entry.contact_time).total_seconds()
                 max_silence_time = self.system[SystemKeys.MAX_SILENCE_TIME]
                 if silence_time > max_silence_time and max_silence_time != -1:
@@ -246,6 +249,98 @@ class AlertEmail(object):
 
         if self.email_limit_manager.email_limit_reached(event_entry, eventid):
             return
+
+        print "loc 1"
+        sendit = True
+        if self.system[SystemKeys.EMAIL_MUTE_RECONNECT_SECONDS]:
+            # Potentially mute the connect or reconnect emails.
+            import pdb; pdb.set_trace()
+            if event_entry.key == EventControl.AGENT_DISCONNECT:
+                self._mute_dis_check(data, to_emails, bcc, subject, message)
+                # If the event is emailed, it is done there,
+                # after a delay
+                return
+
+            elif event_entry.key == EventControl.AGENT_COMMUNICATION:
+                sendit = self._mute_reconn_check(data)
+
+        if sendit:
+            self._do_send(to_emails, bcc, subject, message)
+
+    def _mute_reconn_check(self, data):
+        """
+            Send a reconnect event email only if the reconnect happened
+            after EMAIL_MUTE_RECONNECT_SECONDS.
+        """
+        if not 'last_disconnect_time' in data:
+            self.log.error("_mute_reconn_check: missing 'last_disconnect_time' "
+                           "in data '%s'", str(data))
+
+
+        timedelta = datetime.utcnow() - data['last_disconnect_time']
+        if timedelta <= self.system[SystemKeys.EMAIL_MUTE_RECONNECT_SECONDS]:
+            print "reconnect too soon"
+            return False    # Don't send the reconnect email
+        print "will send reconnect email"
+        return True         # send the reconenct email
+
+    def _mute_dis_check(self, data, to_emails, bcc, subject, message):
+        """For poor network connections between an agent and the controller,
+           don't send email or disconnect or reconnect events that occur
+           in less than EMAIL_MUTE_RECONNECT_SECONDS, if set.
+        """
+        # pylint: disable=too-many-arguments
+
+        if not self.system[SystemKeys.EMAIL_MUTE_RECONNECT_SECONDS]:
+            return True
+
+        tobj = threading.Thread(target=self._dis_thread,
+                args=(data, to_emails, bcc, subject, message))
+        tobj.daemon = True
+        print 'starting thread'
+        tobj.start()
+
+    def _dis_thread(self, data, to_emails, bcc, subject, message):
+        """
+            Send a disconnect event email only if:
+                  The agent is still disconnected after the agent is
+                  still disconnected after
+                      EMAIL_MUTE_RECONNECT_SECONDS
+        """
+        # pylint: disable=too-many-arguments
+
+        print "dis thread for", subject, message
+        if not 'last_disconnect_time' in data:
+            self.log.error("_dis_thread: Missing 'last_disconnect_time' "
+                           "in data %s", str(data))
+            return
+        orig_last_disconnect_time = data['last_disconnect_time']
+
+        time.sleep(self.system[SystemKeys.EMAIL_MUTE_RECONNECT_SECONDS])
+        print "woke up"
+        if not 'agentid' in data:
+            self.log.error("_dis_thread: missing 'agentid': %s", str(data))
+            return
+        agentid = data['agentid']
+        entry = Agent.get_by_id(agentid)
+        if not entry:
+            self.log.error("_dis_thread: No row for agentid %d", agentid)
+            return
+        if entry.connected():
+            self.log.debug("_dis_thread: agentid %d now connected.", agentid)
+            return
+
+        if orig_last_disconnect_time != entry.disconnect_time():
+            self.log.debug("_dis_thread: agentid %d disconnect time changed. "
+                           "Ignoring.", agentid)
+            return
+
+        self.log.debug("_dis_thread: sending email for agentid %d: %s",
+                       agentid, subject)
+        self._do_send(to_emails, bcc, subject, message)
+
+    def _do_send(self, to_emails, bcc, subject, message):
+        # pylint: disable=too-many-locals
 
         # Convert from Unicode to utf-8
         message = message.encode('utf-8')    # prevent unicode exception
@@ -290,6 +385,8 @@ class AlertEmail(object):
                 " port: %d",
                 message, ex, self.smtp_server, self.smtp_port)
             return
+
+        print 'email sent', subject
 
         self.log.info("Emailed alert: To: '%s' Subject: '%s', message: '%s'",
                                                 str(all_to), subject, message)
