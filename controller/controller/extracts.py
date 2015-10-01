feb200f994ac687b02a8296e901caa61533a1118
Tim Flagg <timf@akirisolutions.com>
2015-09-30 17:06:49 -0700

    PD-5310: Extract tardiness alerting

--- controller/controller/extracts.py
+++ controller/controller/extracts.py
@@ -1,5 +1,6 @@
-import time
-from datetime import datetime
+# pylint: enable=relative-import,missing-docstring
+
+"""Manages events for the Tableau background_jobs table."""
 
 from sqlalchemy import Column, BigInteger, Integer, String, DateTime
 from sqlalchemy import UniqueConstraint
@@ -13,25 +14,21 @@ from mixin import BaseMixin, BaseDictMixin
 from cache import TableauCacheManager
 from manager import synchronized
 from odbc import ODBC
-from util import timedelta_total_seconds, utc2local
-from datasources import DataSourceEntry
+from util import timedelta_total_seconds, utc2local, to_hhmmss
 from .system import SystemKeys
+from datasources import DataSourceEntry
 from workbooks import WorkbookEntry
 
-def to_hhmmss(seconds):
-    hours = seconds // (60*60)
-    seconds %= (60*60)
-    minutes = seconds // 60
-    seconds %= 60
-    return "%02i:%02i:%02i" % (hours, minutes, seconds)
 
 class ExtractNotification(object):
+    """Bits used for the notification_state column."""
     EXTRACT_DURATION_WARN = 1 << 0
     EXTRACT_DURATION_ERROR = 1 << 1
     EXTRACT_DELAY_WARN = 1 << 2
     EXTRACT_DELAY_ERROR = 1 << 3
 
 class ExtractEntry(meta.Base, BaseMixin, BaseDictMixin):
+    """The extracts table, from the Tableau background_jobs table."""
     __tablename__ = "extracts"
 
     extractid = Column(BigInteger, unique=True, nullable=False,
@@ -61,40 +58,51 @@ class ExtractEntry(meta.Base, BaseMixin, BaseDictMixin):
     locale = Column(String)
     project_id = Column(Integer)
     system_user_id = Column(Integer)
-    notification_state = Column(Integer, default=0)
+    notification_state = Column(Integer, default=0, nullable=False)
 
     __table_args__ = (UniqueConstraint('envid', 'id'),)
 
     @classmethod
     def get(cls, envid, extract_id, **kwargs):
+        """Returns the row given the envid and id."""
         keys = {'envid':envid, 'id':extract_id}
         return cls.get_unique_by_keys(keys, **kwargs)
 
 class ExtractManager(TableauCacheManager):
+    """Manages alerting for background_jobs."""
 
-    def _add_info(self, table_class, entry):
-        """Look up the extract in the table_class as specified in the args
+    def _add_info(self, entry):
+        """Look up the extract in the entry as specified in the args
            column of the passed entry and fill in information, if available,
            for entry attributes such as:
                 system_user_id
                 project_id
         """
+
+        if entry.subtitle == 'Workbook':
+            obj_class = WorkbookEntry
+        elif entry.subtitle in ('Data Source', 'Datasource',
+                                'RefreshExtractTask'):
+            obj_class = DataSourceEntry
+        else:
+            return
+
         args = entry.args.split()
         if len(args) < 11 or not args[4].isdigit():
             self.log.error("extract add_info: args bad for %s title %s: %s",
-                                table_class.__tablename__,
+                                obj_class.__tablename__,
                                 entry.title, entry.args)
             return
 
         entry_id = int(args[4])
         envid = self.server.environment.envid
 
-        item_entry = table_class.get_newest_by_id(envid, entry_id)
+        item_entry = obj_class.get_newest_by_id(envid, entry_id)
         if not item_entry:
             self.log.error("extract _add_info for %s: No such item "
                            "with title %s, id %d",
-                           table_class.__tablename__,
-                           entry.title, entry_id)
+                           obj_class.__tablename__,
+                           entry.title, entry.id)
             return
 
         entry.system_user_id = item_entry.system_user_id
@@ -102,12 +110,14 @@ class ExtractManager(TableauCacheManager):
 
     @synchronized('extracts')
     def load(self, agent, check_odbc_state=True):
+        """Alerting for background_jobs."""
+
         # pylint: disable=too-many-locals
         envid = self.server.environment.envid
 
         # FIXME
         if check_odbc_state and not self.server.odbc_ok():
-            return {"error": "Cannot run command while in state: %s" % \
+            return {u'error': "Cannot run command while in state: %s" % \
                         self.server.state_manager.get_state()}
 
         try:
@@ -117,7 +127,7 @@ class ExtractManager(TableauCacheManager):
                 "extract prune: Max background job retrieval failed: %s",
                 str(ex))
             # The agent probably disconnected
-            return {"error": \
+            return {u'error': \
                     'max background job retrieval failed: %s' % str(ex)}
 
         stmt = \
@@ -150,7 +160,9 @@ class ExtractManager(TableauCacheManager):
 
         # Get the tableau system's idea of time which may be different
         # than ours (maybe somebody isn't running ntp or equivalent).
-        db_now_utc = self._get_db_now_utc(agent)
+        db_now_utc = agent.odbc.get_db_now_utc()
+
+        extract_thresholds = self._get_thresholds()
 
         # Get the latest rows and row updates here
         session = meta.Session()
@@ -163,15 +175,15 @@ class ExtractManager(TableauCacheManager):
                 session.add(entry)
             odbcdata.copyto(entry)
 
-            data = dict(agent.todict().items())
+            data = dict(agent.todict().items() + extract_thresholds.items())
             self._process(data, db_now_utc, entry)
             session.commit()    # can move this after proven reliable
 
-        self._check_existing_unfinished(agent)
+        self._check_existing_unfinished(agent, extract_thresholds)
 
         return {u'status': 'OK', u'count': len(datadict[''])}
 
-    def _check_existing_unfinished(self, agent):
+    def _check_existing_unfinished(self, agent, extract_thresholds):
         """Go through our unfinished extracts db to see if any have exceeded
            their start delay or duration thresholds.
         """
@@ -181,51 +193,23 @@ class ExtractManager(TableauCacheManager):
             filter(ExtractEntry.progress != 100).\
             all()
 
-        db_now_utc = self._get_db_now_utc(agent)
+        db_now_utc = agent.odbc.get_db_now_utc()
 
         for row in rows:
-            data = dict(agent.todict().items())
+            data = dict(agent.todict().items() + extract_thresholds.items())
             self._process(data, db_now_utc, row)
             session.commit()    # can move this after proven reliable
 
-    # Fixme: Move to a general db file
-    def _get_db_now_utc(self, agent):
-        """
-            Get the tableau postgres database's idea of the current
-            time which may differ from our idea of the current time.
-        """
-        stmt = "select now()"
-        datadict = agent.odbc.execute(stmt)
-
-        if 'error' in datadict or '' not in datadict:
-            return datadict
-
-        time_rows = datadict['']
-        if not len(time_rows) or not len(time_rows[0]):
-            self.log.error("extract load.  Missing db time now: %s", time_rows)
-            return datetime.utcnow()
-
-        # Comes back something like "2015-09-29 18:19:08.156985+00"
-        time_str = time_rows[0][0]
-        if time_str.find('.'):
-            time_str = time_str.split('.')[0]
-        try:
-            struct = time.strptime(time_str, "%Y-%m-%d %H:%M:%S")
-        except ValueError:
-            self.log.error("extract load.  Bad db now: %s", time_str)
-            return datetime.utcnow()
-
-        return datetime.fromtimestamp(time.mktime(struct))
-
     def _process(self, data, db_now_utc, entry):
-        """Send events are appropriate:
+        """Send events as appropriate:
             - delayed start
             - duration exeeded
             - EXTRACT OK
             - EXTRACT FAILED
         """
+
         if entry.notification_state is None:
-            entry.notification_stateis = 0
+            entry.notification_state = 0
 
         if not entry.started_at:
             self._check_start_delay(data, db_now_utc, entry)
@@ -241,18 +225,18 @@ class ExtractManager(TableauCacheManager):
         time_since_created = timedelta_total_seconds(db_now_utc,
                                                         entry.created_at)
         # Check to see if the extract STARTED late.
-        if self.system[SystemKeys.EXTRACT_DELAY_ERROR] and \
+        if data[SystemKeys.EXTRACT_DELAY_ERROR] and \
                 (time_since_created >= \
-                    self.system[SystemKeys.EXTRACT_DELAY_ERROR]) and \
+                    data[SystemKeys.EXTRACT_DELAY_ERROR]) and \
                         not entry.notification_state & \
                             ExtractNotification.EXTRACT_DELAY_ERROR:
             data['extract_delay_seconds'] = time_since_created
             self._eventgen(EventControl.EXTRACT_DELAY_ERROR, data, entry)
             entry.notification_state |= \
                                     ExtractNotification.EXTRACT_DELAY_ERROR
-        elif self.system[SystemKeys.EXTRACT_DELAY_WARN] and \
+        elif data[SystemKeys.EXTRACT_DELAY_WARN] and \
                     (time_since_created >= \
-                        self.system[SystemKeys.EXTRACT_DELAY_WARN]) and \
+                        data[SystemKeys.EXTRACT_DELAY_WARN]) and \
                             not entry.notification_state & \
                                 (ExtractNotification.EXTRACT_DELAY_WARN | \
                                 ExtractNotification.EXTRACT_DELAY_ERROR):
@@ -274,18 +258,18 @@ class ExtractManager(TableauCacheManager):
             # Compute how long it's been running so far
             run_time = int((db_now_utc - entry.started_at).\
                             total_seconds())
-        if self.system[SystemKeys.EXTRACT_DURATION_ERROR] and \
+        if data[SystemKeys.EXTRACT_DURATION_ERROR] and \
                 (run_time >=
-                    self.system[SystemKeys.EXTRACT_DURATION_ERROR]) and \
+                    data[SystemKeys.EXTRACT_DURATION_ERROR]) and \
                         not entry.notification_state & \
                             ExtractNotification.EXTRACT_DURATION_ERROR:
             data['extract_duration_seconds'] = run_time
             self._eventgen(EventControl.EXTRACT_DURATION_ERROR, data, entry)
             entry.notification_state |= \
                                 ExtractNotification.EXTRACT_DURATION_ERROR
-        elif self.system[SystemKeys.EXTRACT_DURATION_WARN] and \
+        elif data[SystemKeys.EXTRACT_DURATION_WARN] and \
                 (run_time >= \
-                            self.system[SystemKeys.EXTRACT_DURATION_WARN]) and \
+                            data[SystemKeys.EXTRACT_DURATION_WARN]) and \
                         not entry.notification_state & \
                             (ExtractNotification.EXTRACT_DURATION_WARN |
                              ExtractNotification.EXTRACT_DURATION_ERROR):
@@ -316,6 +300,8 @@ class ExtractManager(TableauCacheManager):
         return str(value)
 
     def _last_background_jobs_id(self, agent):
+        """Returns the most recent id from the Tableau background_jobs
+           table or 0 if it is empty."""
         stmt = "SELECT MAX(id) FROM background_jobs"
         data = agent.odbc.execute(stmt)
         if 'error' in data:
@@ -340,18 +326,14 @@ class ExtractManager(TableauCacheManager):
 
     # FIXME: add project_id? maybe job_name?
     def _eventgen(self, key, data, entry):
-        # Placeholder to be set by the next functions.
+        """Fill in information into the data dictionary and send the event."""
+        # Placeholder to be set by the next steps.
         entry.system_user_id = -1
 
-        if entry.subtitle == 'Workbook':
-            self._add_info(WorkbookEntry, entry)
-        if entry.subtitle in ('Data Source', 'Datasource',
-                              'RefreshExtractTask'):
-            self._add_info(DataSourceEntry, entry)
+        self._add_info(entry)
 
         data = dict(data.items() + entry.todict().items())
 
-        self._add_minutes(data)
         if entry.completed_at is not None and entry.started_at is not None:
             duration = timedelta_total_seconds(entry.completed_at,
                                                entry.started_at)
@@ -372,31 +354,34 @@ class ExtractManager(TableauCacheManager):
             userid = None
 
         self.server.event_control.gen(key, data,
-                                             userid=userid,
-                                             site_id=data['site_id'],
-                                             timestamp=timestamp)
+                                      userid=userid,
+                                      site_id=data['site_id'],
+                                      timestamp=timestamp)
 
-    def _add_minutes(self, data):
-        """Add extract_delay/duration_warn/error_minutes to the
-           data dictionary.
+    def _get_thresholds(self):
         """
-        if self.system[SystemKeys.EXTRACT_DELAY_WARN]:
-            data['extract_delay_warn_minutes'] = \
-                        self.system[SystemKeys.EXTRACT_DELAY_WARN] / 60
-        if self.system[SystemKeys.EXTRACT_DELAY_ERROR]:
-            data['extract_delay_error_minutes'] = \
-                        self.system[SystemKeys.EXTRACT_DELAY_ERROR] / 60
+            Return a dictionary with cached extract-* thresholds,
+            so we don't reread the database for system table info.
+        """
 
-        if self.system[SystemKeys.EXTRACT_DURATION_WARN]:
-            data['extract_duration_warn_minutes'] = \
-                        self.system[SystemKeys.EXTRACT_DURATION_WARN] / 60
-        if self.system[SystemKeys.EXTRACT_DURATION_ERROR]:
-            data['extract_duration_error_minutes'] = \
+        return {
+            # These are cached and used by us.
+            SystemKeys.EXTRACT_DURATION_WARN:
+                                self.system[SystemKeys.EXTRACT_DURATION_WARN],
+            SystemKeys.EXTRACT_DURATION_ERROR:
+                                self.system[SystemKeys.EXTRACT_DURATION_ERROR],
+            SystemKeys.EXTRACT_DELAY_WARN:
+                                self.system[SystemKeys.EXTRACT_DELAY_WARN],
+            SystemKeys.EXTRACT_DELAY_ERROR:
+                                self.system[SystemKeys.EXTRACT_DELAY_ERROR],
+
+            # These can be expected in the event texts subject line.
+            'extract_delay_warn_minutes':
+                        self.system[SystemKeys.EXTRACT_DELAY_WARN] / 60,
+            'extract_delay_error_minutes':
+                        self.system[SystemKeys.EXTRACT_DELAY_ERROR] / 60,
+            'extract_duration_warn_minutes':
+                        self.system[SystemKeys.EXTRACT_DURATION_WARN] / 60,
+            'extract_duration_error_minutes':
                         self.system[SystemKeys.EXTRACT_DURATION_ERROR] / 60
-
-    @classmethod
-    def publishers(cls):
-        query = meta.Session.query(UserProfile).\
-            join(ExtractEntry,
-                 UserProfile.system_user_id == ExtractEntry.system_user_id)
-        return query.all()
+        }
